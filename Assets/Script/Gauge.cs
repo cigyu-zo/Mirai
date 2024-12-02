a94e9420e12e492dcbf5c9721cc98ff447309e7a
cigyu-zo <orenji2044@gmail.com>
2024-12-02 12:30:01 +0900

    2024//12/02

--- Assets/Script/Gauge.cs
+++ Assets/Script/Gauge.cs
@@ -7,17 +7,25 @@ using UnityEngine.UI;
 
 public class Gauge : MonoBehaviour
 {
-    public float maxCount = 100;
-    private float pluscount;
     public EnemyShop enemyShop;
-    public Gauge hgauge;
+    [SerializeField] Slider Hissatu;
+   
+    float dashPoint;
     void Start()
     {
-        enemyShop = GetComponent<EnemyShop>();   
+        dashPoint = enemyShop.cooltime;
+        Hissatu.maxValue = enemyShop.cooltime;
+        Hissatu.value = enemyShop.cooltime;
+        enemyShop =GameObject.Find("Ene_Store").GetComponent<EnemyShop>();
+        
     }
 
     void Update()
     {
-     
+        Hissatu.value = enemyShop.count;
+     if(dashPoint < enemyShop.count)
+        {
+            dashPoint++;
+        }
     }
 }
