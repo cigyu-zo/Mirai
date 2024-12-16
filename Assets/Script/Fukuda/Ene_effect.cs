93d4205f4377e22eb5d23c7007cd7d60f6d9c218
cigyu-zo <orenji2044@gmail.com>
2024-12-16 12:32:01 +0900

    2024/12/16

---
+++ Assets/Script/Fukuda/Ene_effect.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ene_effect : MonoBehaviour
+{
+    private GameObject parent;
+    EnemyShop enemyShop;
+
+    [SerializeField]
+    [Tooltip("Ene_Hissatu")]
+    private ParticleSystem paticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = this.gameObject.transform.parent.gameObject;
+        enemyShop = parent.GetComponent<EnemyShop>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemyShop.cooltime == 100)
+        {
+            ParticleSystem newpaticle = Instantiate(paticle);
+            newpaticle.transform.position = this.transform.position;
+            newpaticle.Play();
+            Destroy(newpaticle.gameObject, 5.0f);
+        }
+    }
+}
